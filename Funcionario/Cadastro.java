ae4c8496d6812885fbde77999799bc3010b8a013
nutelinha02 <arthcc@outlook.com>
2023-08-11 14:03:24 +0000

    candidato incopleto

---
+++ Funcionario/Cadastro.java
@@ -0,0 +1,11 @@
+package Funcionario;
+
+public class Cadastro {
+    int contador;
+    int[] funcio;
+    public Cadastro(int contador){
+        this.funcio = new int[4];
+        this.contador =  contador;
+    }
+   public 
+}
